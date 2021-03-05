629f38a614286e32a8af0ddcc2c19a91ced68fcb
roman1616 <32616350+roman1616@users.noreply.github.com>
2021-03-05 15:27:00 +0700

    refactor: policy editor

--- gateway/src/lib.rs
+++ gateway/src/lib.rs
@@ -1,1 +1,4 @@
 //! MCP Bastion — a local zero-trust MCP JSON-RPC gateway library.
+//!
+//! The binary in `main.rs` is a thin I/O shell around these modules:
+//!   * [`json_scan`] — the honest, single-pass JSON field extractor;
