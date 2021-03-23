498cbd36c9279fc47e33a301968f08160d697ab1
roman1616 <32616350+roman1616@users.noreply.github.com>
2021-03-23 10:33:00 +0700

    build: wildcard matching

--- gateway/src/lib.rs
+++ gateway/src/lib.rs
@@ -4,3 +4,6 @@
 //!   * [`json_scan`] — the honest, single-pass JSON field extractor;
 //!   * [`policy`]    — the line-oriented policy format, matcher and rate limiter;
 //!   * [`redact`]    — argument-value redaction that preserves message bytes;
+//!   * [`audit`]     — structured, one-line-per-event JSON audit records;
+//!   * [`engine`]    — the pure per-message decision pipeline.
+//!
