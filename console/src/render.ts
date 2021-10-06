f3a091255106347503846b61e3ca184dbe786179
roman1616 <32616350+roman1616@users.noreply.github.com>
2021-10-06 11:11:00 +0700

    build: shell detection

--- console/src/render.ts
+++ console/src/render.ts
@@ -12,3 +12,15 @@ import type { ParsedPolicy, PolicyIssue } from "./policy.js";
 function bar(count: number, max: number, width = 24): string {
   if (max <= 0) return "";
   const filled = Math.round((count / max) * width);
+  return "#".repeat(filled) + ".".repeat(Math.max(0, width - filled));
+}
+
+function pad(s: string, n: number): string {
+  return s.length >= n ? s : s + " ".repeat(n - s.length);
+}
+
+function padLeft(s: string, n: number): string {
+  return s.length >= n ? s : " ".repeat(n - s.length) + s;
+}
+
+/** Render the aggregate as a human-readable multi-line report. */
